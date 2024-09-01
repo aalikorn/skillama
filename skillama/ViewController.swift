43d68a8ae47be54c7f0c32e86a16e93a30147861
Daria Nikolaeva <nikolaevdaaa@gmail.com>
2024-09-01 12:59:50 +0300

    Initial Commit

---
+++ skillama/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  skillama
+//
+//  Created by Даша Николаева on 01.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
